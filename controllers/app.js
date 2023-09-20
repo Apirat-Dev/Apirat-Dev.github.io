5c9aa4c8f9739e8639d42508fe9bc8f09765c2fb
Apirat Lordkaew <Apirat.Lordkaew@microchip.com>
2023-09-20 14:01:54 +0700

    Update All

--- controllers/app.js
+++ controllers/app.js
@@ -1,5 +1,13 @@
-angular.module('GalleryPage', [])
-
+angular
+.module('GalleryPage', [])
 .controller('GalleryController', function GalleryController($scope){
   $scope.data = [{name: "test", lastname: "test2"}];
 })
+
+window.onload = function(){
+  try{
+    new MaterialPhotoGallery(document.querySelector('.m-p-g'));
+  } catch (e){
+    alert(e);
+  }
+}
